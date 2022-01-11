6fe9d5f56e108edd7282000d3dfc8734ba8225af
Ben Chatelain <ben@octop.ad>
2022-01-10 18:27:56 -0700

    🖼 Add initial swift package

---
+++ Sources/Ditto.test/Ditto_test.swift
@@ -0,0 +1,6 @@
+public struct Ditto_test {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/Ditto.testTests/Ditto_testTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Ditto_test
+
+final class Ditto_testTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Ditto_test().text, "Hello, World!")
+    }
+}
